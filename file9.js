40442fd720095394957c2b9754ff1437d2e106c4
rohiit0001 <2004rohitvishu@gmail.com>
2026-02-11 15:02:28 +0530

    update

---
+++ file9.js
@@ -0,0 +1,28 @@
+// let a = 10 
+// let name = " mike "
+// let flag = true 
+// let rating = 4.5
+// let score = null
+// let city = "jaipur"
+// console.log(typeof a)
+// console.log(typeof name)
+// console.log(typeof flag)
+// console.log (typeof rating)
+// console.log(typeof score)
+// console.log(typeof city)
+
+
+// let a= "10"
+// console.log(typeof a)
+// //to convert the datatype of  string  to dataype of number means convert the data type from one type to another datatype
+
+// // two ways to convert 
+// //let num = parseInt(a) 
+// let num = Number(a)
+// console.log (typeof num)
+
+
+let a= "true"
+console.log(typeof a)
+let flag = Boolean(a)
+console.log(typeof flag)
