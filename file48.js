c745b9f8fedd4ac8d5e9ee632bb2c078907af188
rohiit0001 <2004rohitvishu@gmail.com>
2026-02-18 12:08:53 +0530

    update

---
+++ file48.js
@@ -0,0 +1,38 @@
+function p1(){
+    return new Promise((resolve ,reject)=>{
+        setTimeout(()=>{
+            resolve("p1 success")
+        })
+    },1000)
+}
+
+function p2(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("p2 Success")
+        })
+    },2000)
+}
+
+function p3(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("p3 success")
+        },3000)
+    })
+}
+
+async function main(){
+    try{
+    // const result= await Promise.all([p1(),p2(),p3()])
+      // const result= await Promise.any([p1(),p2(),p3()])
+        // const result= await Promise.race([p1(),p2(),p3()])
+        const result = await Promise.allSettled([p1(),p2(),p3()])
+    console.log(result)
+}
+catch(err){
+    console.log(err)
+}
+}
+
+main()
---
+++ file50.js
@@ -0,0 +1,13 @@
+// const student ={
+//     name:"rohit",
+//     age:21
+// }
+// console.log(student)
+// const result = JSON.stringify(student)
+// console.log(result)
+
+
+const student ='{"name":"rohit","age":21}'
+console.log(student)
+const result =JSON.parse(student)
+console.log(result)
